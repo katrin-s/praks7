fd8842830d50bad5678e4f012e1efb93da848ab3
Katrin Sooäär <katrin.sooaar@khk.ee>
2018-10-10 11:34:57 +0300

    Lisatud kalkuleerimise võimalus

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // DOM elements
 let kgInput = document.getElementById('kgInput');
-
+let gramOutput = document.getElementById('gramOutput');
+let poundOutput = document.getElementById('poundOutput');
+let ounceOutput = document.getElementById('ounceOutput');
 //Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
-    console.log(kg);
+    gramOutput.innerHTML = `${kg * 1000} g`;
+    poundOutput.innerHTML = `${kg * 2.205} lbs`;
+    ounceOutput.innerHTML = `${kg * 35.274} oz`;
 });
