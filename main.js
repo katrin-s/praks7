02e7fa02924c62e5e505cefd53870904d90408e7
Katrin Sooäär <katrin.sooaar@khk.ee>
2018-10-10 11:13:39 +0300

    Lisatud JS event listener

---
+++ main.js
@@ -0,0 +1,8 @@
+// DOM elements
+let kgInput = document.getElementById('kgInput');
+
+//Listening to events
+kgInput.addEventListener('input', event => {
+    let kg = event.target.value;
+    console.log(kg);
+});
